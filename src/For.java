f17aa7f47c89fdf3fb50364f0c69a6df780f3dce
takahashi1106 <tk2696274@gmail.com>
2022-06-27 15:37:46 +0900

    [update]

--- src/For.java
+++ src/For.java
@@ -2,5 +2,11 @@ public class For {
   public static void main(String[]args){
     //for( int n = 1, x = 2 ; n <= 8 ; n++, x *= 2 )
     //System.out.println( "2の" + n + "乗=" + x );
+    
+    //int x =1;
+    //for(int n =2; n <= 7;n++)
+    //x *= n;
+    //System.out.println(x);
+
   }
 }
