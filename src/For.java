083b3e1c55db77343558f2316fce5c2a07fb0a5d
takahashi1106 <tk2696274@gmail.com>
2022-06-27 15:25:17 +0900

    [update]

--- src/For.java
+++ src/For.java
@@ -1,6 +1,6 @@
 public class For {
   public static void main(String[]args){
-    for (int i= 0; i <10; ++i)
-      System.out.println("SPAM");
+    //for( int n = 1, x = 2 ; n <= 8 ; n++, x *= 2 )
+    //System.out.println( "2の" + n + "乗=" + x );
   }
 }
