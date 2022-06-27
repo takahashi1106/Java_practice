eb1a53c3ed6c2cf430b51f46a6526edf9642d9d1
takahashi1106 <tk2696274@gmail.com>
2022-06-27 14:53:56 +0900

    [update]

--- src/Chapter02.java
+++ src/Chapter02.java
@@ -1,14 +1,9 @@
 public class Chapter02 {
   public static void main(String[]args){
-    int x,y,b;
-    x = 3;
-    y = 7;
-    
-    b = x;
-    x = y;
-    y = b;
-
-    System.out.println( "x=" + x + ",y=" + y );
+    int x = 3;
+    System.out.println( "x=" + x );
+    System.out.println( "x=" + x*x );
+    System.out.println( "x=" + x*x*x );
 }
 }
 
