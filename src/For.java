493209061cf7e5e080788458dd871ea87548901f
takahashi1106 <tk2696274@gmail.com>
2022-06-28 17:45:11 +0900

    [update]

--- src/For.java
+++ src/For.java
@@ -3,10 +3,10 @@ public class For {
     //for( int n = 1, x = 2 ; n <= 8 ; n++, x *= 2 )
     //System.out.println( "2の" + n + "乗=" + x );
     
-    //int x =1;
-    //for(int n =2; n <= 7;n++)
-    //x *= n;
-    //System.out.println(x);
+    int x =1;
+    for(int n =2; n <= 7;n++)
+    x *= n;
+    System.out.println(x);
 
     
   }
