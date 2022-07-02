da3b4da95eafe09868ef9325b6dd260e3fd94083
takahashi1106 <tk2696274@gmail.com>
2022-07-02 18:15:05 +0900

    [update]

--- src/For.java
+++ src/For.java
@@ -3,11 +3,31 @@ public class For {
     //for( int n = 1, x = 2 ; n <= 8 ; n++, x *= 2 )
     //System.out.println( "2の" + n + "乗=" + x );
     
-    int x =1;
+    /*int x =1;
     for(int n =2; n <= 7;n++)
     x *= n;
-    System.out.println(x);
+    System.out.println(x);*/
 
+  //SPAMを10回表示する
+    /*for (int i = 1; i<=10; i++){
+      System.out.println("SPAM");
+    }*/
     
+  //九九、三の段（ 3 ～ 27 の 3 の倍数）を表示する
+    /*for (int i =1;i<=9;i++){
+      System.out.println(i*3);
+    }*/
+
+  //2 の 1 乗から 8 乗までを計算し表示する
+    /*int x = 1;
+    for (int i = 1; i <=8; i++){
+      x *= 2;
+      System.out.println("2の"+i+"乗は"+x);
+    }*/
+
+    /*for (int i =1, x=2; i<=8; i++, x*=2)
+      System.out.println("2の"+i+"乗は"+x);*/
+
+
   }
 }
