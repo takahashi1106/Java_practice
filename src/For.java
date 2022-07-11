d3073cd361a88f6f367f4918d838735828fa20da
takahashi1106 <tk2696274@gmail.com>
2022-07-11 20:46:52 +0900

    [update]

--- src/For.java
+++ src/For.java
@@ -1,24 +1,24 @@
 public class For {
   public static void main(String[]args){
-    //for( int n = 1, x = 2 ; n <= 8 ; n++, x *= 2 )
-    //System.out.println( "2の" + n + "乗=" + x );
+//for( int n = 1, x = 2 ; n <= 8 ; n++, x *= 2 )
+//System.out.println( "2の" + n + "乗=" + x );
     
     /*int x =1;
     for(int n =2; n <= 7;n++)
     x *= n;
     System.out.println(x);*/
 
-  //SPAMを10回表示する
+//SPAMを10回表示する
     /*for (int i = 1; i<=10; i++){
       System.out.println("SPAM");
     }*/
     
-  //九九、三の段（ 3 ～ 27 の 3 の倍数）を表示する
+//九九、三の段（ 3 ～ 27 の 3 の倍数）を表示する
     /*for (int i =1;i<=9;i++){
       System.out.println(i*3);
     }*/
 
-  //2 の 1 乗から 8 乗までを計算し表示する
+//2 の 1 乗から 8 乗までを計算し表示する
     /*int x = 1;
     for (int i = 1; i <=8; i++){
       x *= 2;
@@ -28,24 +28,24 @@ public class For {
     /*for (int i =1, x=2; i<=8; i++, x*=2)
       System.out.println("2の"+i+"乗は"+x);*/
 
-  //7 の階乗を計算し、表示するプログラム
+//7 の階乗を計算し、表示するプログラム
     /*int x = 1;
     for (int i = 2; i <=7; i++){
       x *= i;
     }
     System.out.println(x);*/
 
-  //整数を 10 回入力し、平均値を求めるプログラムを作成しなさい。
-  //要らないint i = (int)(Math.random()*10 +1);
+//整数を 10 回入力し、平均値を求めるプログラムを作成しなさい。
+//要らないint i = (int)(Math.random()*10 +1);
     /*int sum = 0;
     for (int i = 1; i <=10; i++){
     sum += i;
     }
     System.out.println(sum/10);*/
 
-  //1〜10までの整数をカンマ区切りで横並びで表示させる
-  //(10にはカンマをつけない)
-  /*int []a = {1,2,3,4,5,6,7,8,9,10};
+//1〜10までの整数をカンマ区切りで横並びで表示させる
+//(10にはカンマをつけない)
+/*int []a = {1,2,3,4,5,6,7,8,9,10};
     for(int i = 0; i < a.length; i++){
       System.out.print(a[i]);
       if(i < a.length - 1){
@@ -53,7 +53,7 @@ public class For {
       }
     }*/
 
-  //1〜10までに偶数が何個あるか、また偶数の合計は幾つになるか表示させる
+//1〜10までに偶数が何個あるか、また偶数の合計は幾つになるか表示させる
   /*int count = 0;
   int sum = 0;
     for(int i = 1; i <=10; i++) {
@@ -65,7 +65,7 @@ public class For {
       System.out.println(count);
       System.out.println(sum);*/
 
-  //整数、0 か 1 を 10 回入力する。これを対戦成績と考え、0 を負け、1 を勝ちとして、勝ちの総数、負けの総数を表示するプログラム
+//整数、0 か 1 を 10 回入力する。これを対戦成績と考え、0 を負け、1 を勝ちとして、勝ちの総数、負けの総数を表示するプログラム
    /*int win = 0;
    int lose = 0;
 
@@ -80,8 +80,8 @@ public class For {
     System.out.println("勝ちは"+win+"回");
     System.out.println("負けは"+lose+"回");*/
 
-  //要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の
-  //奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる
+//要素数20の配列を宣言し、要素に0から5ずつ加算して代入させる。[0]~[10]の値の
+//奇数のみカンマ区切りで、[11]~[19]の値は偶数をカンマ区切りで表示させる
   /* 
   int a[] = new int[20];
 
@@ -109,7 +109,7 @@ public class For {
             }
         }*/
 
-  //お金の数を数えるプログラム
+//お金の数を数えるプログラム
     /*int X_10000 = 5;
     int X_5000  = 3;
     int X_1000  = 4;
@@ -129,8 +129,8 @@ public class For {
               X_1    *1;
     System.out.println(ans);*/
 
-  //巨人、阪神戦で毎回の得点を入力する。（１回～９回）
-  //入力が終わったら、それぞれの得点とどちらが勝ったかを表示する。
+//巨人、阪神戦で毎回の得点を入力する。（１回～９回）
+//入力が終わったら、それぞれの得点とどちらが勝ったかを表示する。
   /*int hannshin = 0;
   int kyojinn = 0;
 
@@ -154,6 +154,54 @@ public class For {
       } else {
         System.out.println("引き分け");
       }*/
+
+//自然数（正の整数）を 10 回入力し、最大値を求めるプログラムを作成しなさい。
+    /*int[] intArray = {24, 2, 0, 34, 12, 110, 2};
+    int maxNum = intArray[0];
+
+      for (int j : intArray) {
+          if (j > maxNum)
+              maxNum = j;
+      }
+      System.out.println("Maximum number = " + maxNum);*/
+
+  /*int maxNum = 0;
+
+    for(int i = 1; i <= 10; i++){
+      if (i > maxNum){
+      maxNum = i;
+      }
+    }
+    System.out.println(maxNum);*/
+
+//整数を 10 回入力し、最小値を求めるプログラムを作成しなさい。
+
+  /*int maxNum = 10;
+    for(int i = 10; i >= 1; i--){
+      if (i < maxNum){
+      maxNum = i;
+      }
+    }
+    System.out.println(maxNum);*/
+
+  /*int[] a = {20,40,18,6,25,10};
+  int minMax = a[0];
+    
+    for (int b : a){
+      if (b < minMax){
+        minMax = b;
+      }
+    }
+    System.out.println(minMax);*/
+
+//個数を示す数値を入力し、その個数分だけ‘*’を表示するプログラム
+
+    /*int count = 10;
+
+    for (int i = 1; i <= count; i++){
+      System.out.print(i+"*"+"個");
+      System.out.print(" ");
+    }*/
 
   }
 }
